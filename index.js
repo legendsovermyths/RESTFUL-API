f72acce261e7522c2032d0673867909580475479
legendsovermyths <maskwolf2000@gmail.com>
2021-06-11 14:17:05 +0530

    API CRUD

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+
+const app = express();
+
+app.set("view engine", "ejs");
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
+  })
+);
+app.use(express.static("public"));
+
+//TODO
+mongoose.connect("mongodb://localhost:27017/infoDB", { useNewUrlParser: true });
+
+const infoSchema = {
+  title: String,
+  content: String,
+};
+
+const Info = mongoose.model("Info", infoSchema);
+
+app.get("/get", function (req, res) {
+  Info.find(function (err, result) {
+    if (!err) {
+      console.log(result[1]);
+      console.log(result);
+    }
+  });
+});
+
+app.post("/articles", function (req, res) {
+  console.log(req.body.title);
+  console.log(req.body.content);
+  const newInfo = new Info({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  newInfo.save(function (err) {
+    if (!err) {
+      res.send("Article saved successfully");
+    } else {
+      res.send(err);
+    }
+  });
+});
+app.listen(3000, function () {
+  console.log("Server started on port 3000");
+});
+
+app.get("/", function (res, req) {
+  req.send("Hello Wolrd!");
+});
